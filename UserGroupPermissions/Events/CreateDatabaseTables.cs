4c31e5b0799abd385537257caf7507a545eb9635
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-07-24 17:26:48 -0700

    Added some exception handling to handle insufficient database permissions.

---
+++ UserGroupPermissions/Events/CreateDatabaseTables.cs
@@ -0,0 +1,49 @@
+﻿namespace UserGroupPermissions.Events
+{
+
+    // Namespaces.
+    using System;
+    using Umbraco.Core;
+    using Umbraco.Core.Logging;
+    using Umbraco.Core.Persistence;
+    using UserGroupPermissions.Models;
+
+
+    /// <summary>
+    /// Creates database tables on application startup.
+    /// </summary>
+    public class CreateDatabaseTables : ApplicationEventHandler
+    {
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Application started.
+        /// </summary>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+
+            // Swallow errors to avoid affecting site if database permissions are insufficient.
+            try
+            {
+
+                // Create UserTypePermissions table if it doesn't exist yet.
+                var db = applicationContext.DatabaseContext.Database;
+                if (!db.TableExist("UserTypePermissions"))
+                {
+                    db.CreateTable<UserTypePermissionRow>(false);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<CreateDatabaseTables>("Unable to create table UserTypePermissions.", ex);
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
