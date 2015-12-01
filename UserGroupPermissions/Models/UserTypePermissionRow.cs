dde1e6973a298cf784995be82db47bf3bcf649d7
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-11-30 22:10:49 -0800

    Fix for SQL error on Azure: https://github.com/Nicholas-Westby/usergrouppermissions/issues/8

--- UserGroupPermissions/Models/UserTypePermissionRow.cs
+++ UserGroupPermissions/Models/UserTypePermissionRow.cs
@@ -1,9 +1,13 @@
 ﻿namespace UserGroupPermissions.Models
 {
     using Umbraco.Core.Persistence;
+    using Umbraco.Core.Persistence.DatabaseAnnotations;
     [TableName("UserTypePermissions")]
+    [PrimaryKey("AutoId", autoIncrement = true)]
     public class UserTypePermissionRow
     {
+        [PrimaryKeyColumn(AutoIncrement = true, Clustered = true)]
+        public long AutoId { get; set; }
         public int NodeId { get; set; }
         public int UserTypeId { get; set; }
         // Should be a char, but changed to string due to Umbraco bug: http://issues.umbraco.org/issue/U4-6787
