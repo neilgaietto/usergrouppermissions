92cd4911caa0e320c9aaf364d8e346e777d1a4af
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-12-28 08:51:11 -0800

    Code cleanup.

--- UserGroupPermissions/Events/AddApplyUserGroupPermissionsToContextMenu.cs
+++ UserGroupPermissions/Events/AddApplyUserGroupPermissionsToContextMenu.cs
@@ -1,40 +1,77 @@
-﻿using Umbraco.Core;
-using Umbraco.Web.Models.Trees;
-using Umbraco.Web.Trees;
-using UserGroupPermissions.ExtensionMethods;
-
-namespace UserGroupPermissions.Events
+﻿namespace UserGroupPermissions.Events
 {
+
+    // Namespaces.
+    using ExtensionMethods;
+    using Umbraco.Core;
+    using Umbraco.Web.Models.Trees;
+    using Umbraco.Web.Trees;
+
+
+    /// <summary>
+    /// Adds the "Apply User Group Permissions" to the context menu for a user.
+    /// </summary>
     public class AddApplyUserGroupPermissionsToContextMenu : ApplicationEventHandler
     {
 
+        #region Constants
+
+        private const string DialogViewPath = "/App_Plugins/UserGroupPermissions/Views/ApplyUserGroupPermissions.html";
+        private const string DialogTitle = "Apply User Group Permissions";
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public AddApplyUserGroupPermissionsToContextMenu()
         {
             TreeControllerBase.MenuRendering += TreeControllerBase_MenuRendering;
         }
 
+        #endregion
+
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event handler when tree is rendering.
+        /// </summary>
         void TreeControllerBase_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
         {
+
+            // Variables.
             var user = sender.Security.CurrentUser;
-            var path = "/App_Plugins/UserGroupPermissions/Views/ApplyUserGroupPermissions.html";
-            var title = "Apply User Group Permissions";
             var treeType = e.QueryStrings.Get("treeType");
             var section = e.QueryStrings.Get("section");
             var validTree = "users".InvariantEquals(treeType) && "users".InvariantEquals(section);
-            var strUserId = e.NodeId ?? "";
+            var strUserId = e.NodeId ?? string.Empty;
             var userId = default(int);
-            var validUserId = int.TryParse(strUserId, out userId) ? userId > 0 : false;
+            var validUserId = int.TryParse(strUserId, out userId)
+                ? userId > 0
+                : false;
             var validUser = user.IsAdmin();
-            if (validTree && validUserId && validUser)
+            var shouldAddMenuItem = validTree && validUserId && validUser;
+
+
+            // Add the menu item?
+            if (shouldAddMenuItem)
             {
                 var menuItem = new MenuItem("ApplyUserGroupPermissions", "Apply Group Permissions")
                 {
                     Icon = "vcard"
                 };
-                menuItem.LaunchDialogView(path, title);
+                menuItem.LaunchDialogView(DialogViewPath, DialogTitle);
                 e.Menu.Items.Add(menuItem);
             }
+
         }
 
+        #endregion
+
     }
+
 }
